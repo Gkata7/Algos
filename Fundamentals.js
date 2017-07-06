0c7707fe22191176e802408f8eecad84b86804f5
Garrett Katashima <gkatashima@gmail.com>
2017-07-06 15:54:59 -0700

    Update

---
+++ Fundamentals.js
@@ -0,0 +1,21 @@
+// 1. Create a function that accepts a number as an input. Return a new array that counts down by one, from the number(as array's zero'th element) down to 0(as the last element). How long is the array?
+// // function countdown(num){
+//   for(var i = 0; i > 0; i--){
+//     newArr.push(i);
+//   }
+//   return newArr.length;
+// }
+
+// 2. For [1,3,5,7,9,13] print values that are greater than it's 2nd value. Return how many values this is.
+function greater(arr){
+  var newArr = [i];
+  for(var i = 0; i < arr.length; i++){
+    if(arr[i] > arr[i]);
+    newArr.push(arr[i])
+  }
+  return newArr;
+}
+
+array = [1,2];
+console.log(0);
+return(2,3)
--- day_1_dojo.js
+++ day_1_dojo.js
@@ -85,7 +85,7 @@ function avg(arr){
     return avgNum;
 }
 
-function greate(arr,Y){
+function greater(arr,Y){
     var count = 0;
 
     for(var i = 0; i < arr.length; i++){
