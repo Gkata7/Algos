02d2a434962152bbf94db6dcdb5c70114ca6647b
Garrett Katashima <gkatashima@gmail.com>
2017-08-05 11:40:57 -0700

    update algos3.js

--- algos3.js
+++ algos3.js
@@ -75,3 +75,48 @@ function drawCenteredStar(num){
 }
 
 drawCenteredStar(50);
+
+
+function threeFives(){
+  var sum = 0;
+  for(var i = 100; i < 4000001; i++){
+    if(i % 3 === 0 || i % 5 === 0){
+      sum += i;
+    }
+  }
+  console.log(sum);
+  return sum;
+}
+
+threeFives();
+
+function coinChange(cents){
+  var pennies = 0;
+  var nickles = 0;
+  var dimes = 0;
+  var quarters = 0;
+  var change = [];
+  for(var i = 0; i < cents; i++){
+    if(cents > 25){
+      cents -= 25;
+      quarters++;
+    }else if(cents > 10){
+      cents -= 10;
+      dimes++;
+    }else if(cents > 5){
+      cents -= 5;
+      nickles++;
+    }else if(cents >= 1){
+      cents -= 1;
+      pennies++;
+    }
+  }
+  change.push("quarters: " + quarters);
+  change.push("dimes: " + dimes);
+  change.push("nickles: " + nickles);
+  change.push("pennies: " + pennies);
+  console.log(change);
+  return change;
+}
+
+coinChange(3);
